52beaca2ef4cc4c2de87097b077f47e4421523ad
diyarammb <you@example.com>
2024-06-04 02:28:45 -0700

    Node

--- app/routes/App.routes.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const websiteRoutes = require("@routes/Website.routes");
-const apiRoutes = require("@routes/Api.routes");
-app.use("/", websiteRoutes);
-app.use("/api", apiRoutes);
